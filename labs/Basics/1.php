95ab79771da0c778416a4a68c389e2e30dab1a05
vivutru <lavivutu@egmail.com>
2026-09-10 16:18:38 -0500

    Refactor: More detailed noted on basic php files

---
+++ labs/Basics/1.php
@@ -0,0 +1,10 @@
+// This is a comment. Comments start with two backslashes
+
+// In php we must end lines with a semicolon
+
+// Below is a basic php script.
+// echo = print whatever is in the brackets
+
+<?php
+echo "hello";
+?>/
---
+++ labs/Basics/5.php
@@ -0,0 +1,14 @@
+// Operators - Arithmetic
+// Note how we can add numeric variables together with the help of operators (+, -, /, *)
+
+<?php
+$price = 500;
+$amount = 3;
+$total = $price * $amount;
+
+echo "Base Price: $" . $price . "<br>";
+echo "Amount: $" . $amount . "<br>";
+echo "Amount: $" . $amount . "<br>";
+echo "Total cost: $" . $total . "<br>";
+
+?>
