b05915614f37fb90258607d5c0827f89bebc2b8a
vivutru <lavivutru@egmail.com>
2026-09-10 16:30:45 -0500

    Edit: Added more detailed notes to basic lab1 phph

--- labs/Basics/1.php
+++ labs/Basics/1.php
@@ -1,6 +1,6 @@
 // This is a comment. Comments start with two backslashes
 
-// In php we must end lines with a semicolon
+// In php we must start our server code with (a greater than sign and a question mark) and end it with (a less than and question mark) all lines in between those open and close php tages must end with a semicolon
 
 // Below is a basic php script.
 // echo = print whatever is in the brackets
