f625486837c36b24ea6a3995a8c0160fd7d9efed
vivutru <lavivutru@gmail.com>
2026-09-15 19:11:54 -0500

    Refactor: Moved basic code to lesson 1 folder

---
+++ labs/lesson_1/1.php
@@ -0,0 +1,10 @@
+// This is a comment. Comments start with two backslashes
+
+// In php we must start our server code with (a greater than sign and a question mark) and end it with (a less than and question mark) all lines in between those open and close php tages must end with a semicolon
+
+// Below is a basic php script.
+// echo = print whatever is in the brackets
+
+<?php
+echo "hello";
+?>/
