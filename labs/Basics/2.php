49a8c4128c4d447812f4c81c76058bb145c79800
vivutru <lavivutru@gmail.com>
2026-09-15 19:14:35 -0500

    Refactor: Fixed basic code folder

--- labs/Basics/2.php
+++
@@ -1,8 +0,0 @@
-// Html tag test
-// Note how we are able to use html tages directly in our php code
-
-<?php
-echo "<h1>PHP test</h1>";
-echo "<p>Php is working correctly</p>";
-echo "<p>Welcome to Web Development III</p>";
-?>
--- labs/Basics/3.php
+++
@@ -1,9 +0,0 @@
-// Variables
-// Variables in PHP start with a $ sign and don't need a declared type because php can infer datatypes based on the way a variable is formated
-
-<?php
-$name = "Alice";
-$role = "student";
-$age = 19;
-echo "{$name} is a {$age} {$role}";
-?>
